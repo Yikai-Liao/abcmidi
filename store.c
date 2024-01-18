e876fa607dd4c155ba42ce7fdd35a59ba181492d
Yikai Liao <lyk-boya@outlook.com>
2024-01-18 14:31:16 +0800

    add log10

--- store.c
+++ store.c
@@ -222,14 +222,14 @@ int setOutFileCreator(char *fileName,unsigned long theType,
 #define strchr index
 #endif
 
-#ifdef ANSILIBS
+//#ifdef ANSILIBS
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-#else
-extern char* strchr();
-extern void reduce();
-#endif
+//#else
+//extern char* strchr();
+//extern void reduce();
+//#endif
 /*int snprintf(char *str, size_t size, const char *format, ...);*/
 int load_stress_parameters(char *);
 
@@ -848,6 +848,32 @@ static void setup_chordnames()
   addchordname("7sus4", 4, list_7sus4);
   addchordname("7sus9", 4, list_7sus9);
   addchordname("5", 2, list_5);
+}
+
+double ln(double x)
+{
+    double old_sum = 0.0;
+    double xmlxpl = (x - 1) / (x + 1);
+    double xmlxpl_2 = xmlxpl * xmlxpl;
+    double denom = 1.0;
+    double frac = xmlxpl;
+    double term = frac;                 // denom start from 1.0
+    double sum = term;
+
+    while ( sum != old_sum )
+    {
+        old_sum = sum;
+        denom += 2.0;
+        frac *= xmlxpl_2;
+        sum += frac / denom;
+    }
+    return 2.0 * sum;
+}
+
+#define LN10 2.3025850929940456840179914546844
+
+double log10( double x ) {
+    return ln(x) / LN10;
 }
 
 void event_init(argc, argv, filename)
